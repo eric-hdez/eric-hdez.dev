db9f543d653717dfe3a056af8679a89ba411a2e7
Eric Hernandez <eric.hdez2001@gmail.com>
2024-01-21 23:51:39 -0800

    forgot to pass drawer state to navbar to reflect the previous change

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -33,7 +33,7 @@ export default function Navigation() {
   return (
     <>
       <Box width="100%" position="fixed" top={0} zIndex={2} bgcolor="background.body">
-        <NavBar {...{ height, handleDrawerOpen }} />
+        <NavBar {...{ height, drawerOpen, handleDrawerOpen }} />
       </Box>
       <Box height={height} />
       <Box width="100%" height="100%">
