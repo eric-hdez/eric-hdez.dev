a1327d2e759bc58866fa0210b0d8ae371ed2c27e
Eric Hernandez <eric.hdez2001@gmail.com>
2024-08-19 21:30:25 -0400

    alright if iframe doesnt fix this resume issue then im out of ideas

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,17 +5,12 @@ export default function Resume() {
   return (
     <>
       <Box height="535px" alignContent="center">
-        <object
-          data="/eric_hdez_resume_dec.pdf"
-          type="application/pdf"
-          width="100%"
-          height="100%"
-        >
+        <iframe src="/eric_hdez_resume_dec.pdf" width="100%" height="100%">
           <Typography textAlign="center">
             This brower does not have a PDF plugin. Use the download button
             above to view the media.
           </Typography>
-        </object>
+        </iframe>
       </Box>
     </>
   );
