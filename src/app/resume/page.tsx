0b444bc309c17b20a9f3eed210ca756d6b6425d1
Eric Hernandez <eric.hdez2001@gmail.com>
2024-08-19 21:24:48 -0400

    aligning text for browsers that cant display resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,14 +4,14 @@ import { Box, Typography } from '@mui/joy';
 export default function Resume() {
   return (
     <>
-      <Box height="535px">
+      <Box height="535px" alignContent="center">
         <object
-          data="../../../public/eric_hdez_resume_dec.pdf"
+          data="/eric_hdez_resume_dec.pdf"
           type="application/pdf"
           width="100%"
           height="100%"
         >
-          <Typography>
+          <Typography textAlign="center">
             This brower does not have a PDF plugin. Use the download button
             above to view the media.
           </Typography>
