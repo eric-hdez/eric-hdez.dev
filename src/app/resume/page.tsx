cb2019a4564b7731bff0b6d3e50e7fe8d1f3b9b1
Eric Hernandez <eric-hdez@Mac.lan>
2025-02-08 00:11:05 -0500

    updating resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,7 +5,7 @@ export default function Resume() {
   return (
     <>
       <Box height="535px" alignContent="center">
-        <iframe src="/eric_hdez_resume_dec.pdf" width="100%" height="100%">
+        <iframe src="/eric_hdez_resume.pdf" width="100%" height="100%">
           <Typography textAlign="center">
             This brower does not have a PDF plugin. Use the download button
             above to view the media.
