f3e918d094af1de3eaa66f994dff811278f866d2
Eric Hernandez <eric-hdez@Mac-383.lan>
2025-04-27 03:36:23 -0400

    adding small updates to font colors and buttons for resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -28,7 +28,7 @@ export default function Example() {
 
   return (
     <>
-      <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+      <dd className="mt-1 text-sm/6 sm:col-span-2 sm:mt-0">
         <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
           {FileList.map(({ name, link }, idx) => (
             <li key={idx} className="flex flex-col sm:flex-row items-center justify-between py-4 pr-5 sm:pl-4 text-sm/6">
@@ -39,14 +39,14 @@ export default function Example() {
               </div>
 
               <div className="flex shrink-0 space-x-4">
-                <a href={link} download className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-400">
+                <a href={link} download className="rounded-md bg-white font-medium underline hover:text-gray-500 hover:cursor-pointer">
                   Download
                 </a>
                 <span aria-hidden="true" className="text-gray-300">|</span>
                 <button
                   type="button"
                   onClick={() => handleView(link, name)}
-                  className="rounded-md bg-white font-medium text-gray-700 hover:text-gray-500 hover:cursor-pointer"
+                  className="rounded-md bg-white font-medium hover:text-gray-500 underline hover:cursor-pointer"
                 >
                   View
                 </button>
@@ -62,21 +62,21 @@ export default function Example() {
           <div className="fixed inset-0 z-50 flex items-center justify-center px-6">
             <div className="relative w-full max-w-5xl h-5/6 bg-white rounded-lg shadow-xl overflow-hidden flex flex-col">
               <div className="p-4 flex justify-between items-center border-b bg-[#fcfcfc]">
-                <div className="pl-2 flex flex-row items-center">
+                <div className="pl-3 flex flex-row items-center">
                   <h3 className="text-lg font-medium truncate">{currentFileName}</h3>
                   <Link
                     href={viewingPdf}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="px-3 py-1"
+                    className="ml-1 p-1 rounded-md hover:cursor-pointer hover:bg-gray-300"
                   >
                     <NewTabIcon />
                   </Link>
                 </div>
-                <div className="flex space-x-2">
+                <div className="flex space-x-2 pr-2">
                   <button
                     onClick={closeViewer}
-                    className="pr-2 py-1 rounded-md"
+                    className="p-1 rounded-md hover:cursor-pointer rounded-md hover:bg-gray-300"
                   >
                     <ExitIcon />
                   </button>
