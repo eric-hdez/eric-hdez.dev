7599c0870cf21835eb811dd59e754c2719cad78b
Eric Hernandez <eric-hdez@Mac-657.lan>
2025-04-28 00:31:32 -0400

    updating title for ucsc its job: software developer

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -6,7 +6,7 @@ const experience = [
     icon: <img src="https://cdn.brandfetch.io/idy68RSCip/w/400/h/400/theme/dark/icon.jpeg?c=1dxbfHSJFAPEGdCLU4o5B" />,
   },
   {
-    title: 'Software Engineer',
+    title: 'Software Developer',
     company: 'UC Santa Cruz IT Services',
     years: 'Jan 2023 - Sept 2023',
     icon: <img src="https://media.licdn.com/dms/image/v2/C4E0BAQEEdPQHFbiGrg/company-logo_100_100/company-logo_100_100/0/1656436364797?e=1750896000&v=beta&t=E8WUjw9I865jqlXRUkQgULJpNNnH0R3-VU1MgLZ1r5E" />,
